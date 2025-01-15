b8b3086e7e09e0be72231d02fc91afd8e8c9bc21
Ankit <anlovanshi@Bestpeers.com>
2025-01-15 17:01:32 +0530

    show all products on home page

--- e_commarce/products/views.py
+++ e_commarce/products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def home(request):
+    # Fetch all products from the database
+    products = Product.objects.all()
+    # Pass the products to the template
+    return render(request, 'home.html', {'products': products})
